fd029e7be27f1c0a6deb5ed61c38487d24b8b2c0
victor-badillo <victor.badillo@udc.es>
2024-11-20 16:14:09 +0100

    Comments, main.ml

--- main.ml
+++ main.ml
@@ -7,9 +7,9 @@ open Lexer;;
 
 (*Recursive function used for reading multiples lines, joininig these ones with a blank space*)
 let rec read_multiline () =
-  let line = read_line () in
-  if String.contains line ';' then line   (*Return input when double semicolon is found*)
-  else line ^ " " ^ read_multiline ()
+  let line = read_line () in              (*Read line*)
+  if String.contains line ';' then line   (*Return input when a colon is found, enough with just one*)
+  else line ^ " " ^ read_multiline ()     (*Keep reading*)
 
 let top_level_loop () =
   print_endline "Evaluator of lambda expressions...";
@@ -17,25 +17,13 @@ let top_level_loop () =
     print_string ">> ";
     flush stdout;
     try
-      let input = read_multiline () in  (*Input from the user*)
+      let input = read_multiline () in          (*Input from user*)
       let trimmed_input = String.trim input in  (*Remove blank spaces and newlines*)
-      if trimmed_input = ";;" then loop ctx     (*Empty input, only typed ";;" *)
-      else(*
-        let tm = s token (from_string input) in
-        let tyTm = typeof ctx tm in
-        (*print_endline ("- : " ^ string_of_ty tyTm ^ " = " ^ string_of_term (eval tm)); *) (* First type and the term*)
-        Format.open_hvbox 0;
-        print_string ("- : " ^ string_of_ty tyTm ^ " =");
-        Format.print_space();
-        pretty_printer (eval tm);
-        Format.close_box ();
-        Format.print_flush(); (*Clean boxes*)
-        print_newline();
-        loop ctx
-        *)
-        let c = s token (from_string (input)) in  (* get the command from the input *)
-        loop (execute ctx c)
-    with
+      if trimmed_input = ";;" then loop ctx     (*Empty input, only typed ";;"*)
+      else
+        let c = s token (from_string (input)) in  (*Get command from input *)
+        loop (execute ctx c)                      (*Execute new command*)
+    with  (*Captured errors*)
        Lexical_error ->
          print_endline "lexical error";
          loop ctx
@@ -48,7 +36,7 @@ let top_level_loop () =
      | End_of_file ->
          print_endline "...bye!!!"
   in
-    loop emptyctx
+    loop emptyctx (*Start loop with an empty context*)
   ;;
 
 top_level_loop ()
